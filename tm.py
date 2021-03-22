af0e8470b7a4b18f83048983e527037ee28e492b
Vladimir <vc@inbox-marketing.ru>
2021-03-22 17:33:30 +0300

    22/03 upd

--- tm.py
+++ tm.py
@@ -21,7 +21,7 @@ len(df)
 # %%
 
 # choosing topic
-data_topic = 'фото фотография шоу фотограф свадьба'
+data_topic = 'работа год график день'
 
 a = pd.DataFrame()
 a['theme'] = df.drop(columns = ['text', 'owner_id']) \
@@ -36,42 +36,40 @@ a = a.loc[a['theme'] == data_topic ] \
     .reset_index(drop = True) \
     .rename({'owner_id':'id'})
 
-# %%
-
 # taking top 25% of the topic data
 #a = a.loc[a['coef'] < 0.95].head(round(len(a)*0.25))
-a = a.loc[a['coef'] < 0.95].head(30000)
+a = a.loc[a['coef'] < 0.95].head(70000)
+
+# %%
+import random
+df_sample = a.sample(frac=1).reset_index(drop=True)
+
+train_data = df_sample.text[:round(len(df_sample)/4)]
+test_data = df_sample.text[round(len(df_sample)/4):]
+
 
 # %%
 # saving the samples 
 file_test = open("test.txt","w")
 file_train = open('train.txt', "w")
 
-for i in a.head(round(len(a)*0.8))['text']:
+for i in train_data:
     file_train.write(i+'. \n')
 file_train.close()
-for i in a.tail(round(len(a)*0.2))['text']:
+for i in test_data:
     file_test.write(i+'. \n')
 file_test.close() 
-# %%
-#a.to_csv('~/BDML/topic_modeling/posts.csv')
-
-
-
 
 
 # %%
 
-
-
-
-# creating markov chains for every topic in the dataset 
 temp = pd.DataFrame()
 temp['theme'] = df.drop(columns = ['text', 'owner_id']) \
     .idxmax(axis=1)
 
 temp['text'] = df['text']
-a = a
+file_multitrain = open('multitrain.txt', "w")
+
 
 for n in df.columns:
 
@@ -81,18 +79,11 @@ for n in df.columns:
   d = d.drop_duplicates(subset = 'text') \
     .sort_values('coef', ascending=False) \
     .reset_index(drop = True) \
-    .loc[d['coef'] < 0.95].head(5000)
+    .loc[d['coef'] < 0.95].head(30000)
+  for i in d['text']:
+    file_multitrain.write(i+'. \n')
+
+file_multitrain.close()
   
-  d['lang'] = d['text'].map(lambda s:langid.classify(s))
-  d = d[d['lang'].str.contains('ru', regex = False) == True]
-  d = d.drop(['lang'], axis = 1)   
-
-  text_model = markov_chain \
-      .Text(d[d['theme'] == n]['text'], state_size= 2, well_formed= True)
-
-  text_model.compile(inplace = True)
-  for i in range(4):
-    print(text_model.make_short_sentence(160))
-
 
 # %%
